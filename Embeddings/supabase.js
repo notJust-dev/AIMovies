b42562aeed09faf2658e25a87a0c40f26d8bc805
Vadim Savin <savinvadim1312@gmail.com>
2023-10-10 17:48:24 +0100

    remove keys

--- Embeddings/supabase.js
+++ Embeddings/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://dojcargjelxedljsxglb.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvamNhcmdqZWx4ZWRsanN4Z2xiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5Njk0NzY5MSwiZXhwIjoyMDEyNTIzNjkxfQ.7xBXyC7nmeycWjQ_0qRgFVYFXFoeupQjtc4IDH8nhFA';
+const supabaseUrl = 'https://<Project Ref>.supabase.co';
+const supabaseKey = '<Supabase Service Role Secret';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,9 +2,8 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://dojcargjelxedljsxglb.supabase.co';
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvamNhcmdqZWx4ZWRsanN4Z2xiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY5NDc2OTEsImV4cCI6MjAxMjUyMzY5MX0.RcfTJG8iEvgNFPrQj1PefFUZvZnmyi91lcif4KHB3LA';
+const supabaseUrl = 'https://<Project ID>.supabase.co';
+const supabaseAnonKey = '<Supabase ANON Key>';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
